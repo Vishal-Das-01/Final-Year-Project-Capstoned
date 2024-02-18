70f460936b6c7e75cff832d47bff8fbdf8604dcb
Syed Hamza Akbar Ali <hamzakbarali@gmail.com>
2024-02-18 14:14:37 +0500

    Developed Login Page: Added Form Container and form inputs

--- src/app/(Unauthenticated)/(1_login_page)/login/page.jsx
+++ src/app/(Unauthenticated)/(1_login_page)/login/page.jsx
@@ -1,3 +1,5 @@
+import styles from "./LoginPage.module.css";
+
 export const metadata = {
   title: "Capstoned Login",
   description: "Capstoned Login Page | Final Year Project (FYP) Management Platform for College & University Students.",
@@ -5,8 +7,15 @@ export const metadata = {
 
 export default function LoginPage(props){
 	return (
-		<div className="w-full h-full flex items-center justify-center">
-			Login
+		<div className="w-full h-full flex items-center justify-center border-2 border-black">
+			<div className={`${styles.secondaryContainer} flex flex-col items-center justify-center rounded-lg `}>
+				<div className={`${styles.formContainer} flex flex-col items-center justify-center w-full h-full border-2 border-blue-500`}>
+					<form className="flex flex-col">
+						<input type="email" />
+						<input type="password" />
+					</form>
+				</div>
+			</div>
 		</div>
 	);
 }
