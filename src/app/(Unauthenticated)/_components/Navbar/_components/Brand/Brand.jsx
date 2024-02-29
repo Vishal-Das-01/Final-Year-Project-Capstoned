52f28f873fa329e1c0965f7227043d3f6add51cc
Syed Hamza Akbar Ali <hamzakbarali@gmail.com>
2024-03-01 01:40:30 +0500

    Developed landing page: added logo to landing page navbar

--- src/app/(Unauthenticated)/_components/Navbar/_components/Brand/Brand.jsx
+++ src/app/(Unauthenticated)/_components/Navbar/_components/Brand/Brand.jsx
@@ -1,11 +1,15 @@
 import Link from "next/link";
+import Image from "next/image";
 
 export default function Brand({children, href}){
 	return (
-		<Link href={href}>
-			<p className="font-bold text-xl font-exo2 text-black hover:text-neutral-500">
-				{children}
-			</p>
-		</Link>
+		<div className={`flex flex-row items-center justify-center`}>
+			<Image src={`/logo2.png`} height={100} width={100} className={``}/>
+			<Link href={href}>
+				<p className="font-bold text-xl font-exo2 text-black hover:text-neutral-500">
+					{children}
+				</p>
+			</Link>
+		</div>
 	);
 }
