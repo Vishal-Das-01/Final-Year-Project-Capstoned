ac8db73b61490b37be9b934ef634d4d37bf5e4b8
Syed Hamza Akbar Ali <hamzakbarali@gmail.com>
2024-03-01 01:03:19 +0500

    Developed Landing Page: Changed design of navbar action link button and enter capstoned button. Removed files from public folder

--- src/app/(Unauthenticated)/(0_landing_page)/_components/EnterCapstonedBtn/EnterCapstonedBtn.jsx
+++ src/app/(Unauthenticated)/(0_landing_page)/_components/EnterCapstonedBtn/EnterCapstonedBtn.jsx
@@ -7,7 +7,7 @@ export default function EnterCapstonedBtn({children, href}){
 			
 			<Link href={href}>
 			
-				<div className={`${styles.enterCapstonedBtn} py-2 px-4 bg-blue-500 rounded-lg text-white border-4 border-blue-500 hover:bg-white hover:text-blue-500 hover:border-4 hover:border-blue-500`}>
+				<div className={`${styles.enterCapstonedBtn} py-2 px-4 bg-white rounded-lg text-black border-4 border-black hover:bg-black hover:text-white hover:border-4 hover:border-black`}>
 					
 					<p className={`text-xl font-montserrat font-semibold`}>
 						{children}
@@ -20,3 +20,7 @@ export default function EnterCapstonedBtn({children, href}){
 		</div>
 	);
 }
+
+/*
+<div className={`${styles.enterCapstonedBtn} py-2 px-4 bg-blue-500 rounded-lg text-white border-4 border-blue-500 hover:bg-white hover:text-blue-500 hover:border-4 hover:border-blue-500`}>
+*/
--- src/app/(Unauthenticated)/_components/Navbar/_components/ActionButtonLink/ActionButtonLink.jsx
+++ src/app/(Unauthenticated)/_components/Navbar/_components/ActionButtonLink/ActionButtonLink.jsx
@@ -4,11 +4,15 @@ import styles from "./ActionButtonLink.module.css";
 export default function ActionButtonLink({children, href, filled}){
 	return (
 		<Link href={href}>
-			<div className={`${styles.actionButtonLinkDiv} flex items-center justify-center px-4 border-4 border-blue-500 rounded-lg mx-4 ${filled ? "bg-blue-500 hover:bg-blue-400 hover:border-blue-400" : "bg-white hover:bg-slate-200"}`}>
-				<p className={`text-base font-semibold font-montserrat ${filled ? "text-white" : "text-blue-500"} }`}>
+			<div className={`${styles.actionButtonLinkDiv} flex items-center justify-center px-4 rounded-lg mx-4 border-4 border-black hover:bg-black hover:border-black hover:text-white `}>
+				<p className={`text-base font-semibold font-montserrat text-black}`}>
 					{children}
 				</p>
 			</div>
 		</Link>
 	);
 }
+
+/* 
+<div className={`${styles.actionButtonLinkDiv} flex items-center justify-center px-4 border-4 border-blue-500 rounded-lg mx-4 ${filled ? "bg-blue-500 hover:bg-blue-400 hover:border-blue-400" : "bg-white hover:bg-slate-200"}`}>
+*/
