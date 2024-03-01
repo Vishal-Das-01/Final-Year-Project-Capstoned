1eef7f367dee7bae82683fa1465f11ba9a2e93ef
Syed Hamza Akbar Ali <hamzakbarali@gmail.com>
2024-03-01 14:10:35 +0500

    Developed Landing Page Navbar: Created and added new component called CapstonedLogo

--- src/app/(Unauthenticated)/_components/Navbar/_components/Brand/Brand.jsx
+++ src/app/(Unauthenticated)/_components/Navbar/_components/Brand/Brand.jsx
@@ -1,15 +1,22 @@
 import Link from "next/link";
 import Image from "next/image";
+import CapstonedLogo from "@/components/CapstonedLogo/CapstonedLogo.jsx";
+import styles from "./Brand.module.css";
 
 export default function Brand({children, href}){
 	return (
-		<div className={`flex flex-row items-center justify-center`}>
-			<Image src={`/logo2.png`} height={100} width={100} className={``}/>
+		<div className={`flex flex-row items-center relative justify-center`}>
+			
+			<CapstonedLogo />
+
 			<Link href={href}>
+				
 				<p className="font-bold text-xl font-exo2 text-black hover:text-neutral-500">
 					{children}
 				</p>
+			
 			</Link>
+		
 		</div>
 	);
 }
