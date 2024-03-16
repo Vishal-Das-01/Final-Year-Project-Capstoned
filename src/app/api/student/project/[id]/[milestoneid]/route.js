a373e8724eef906def4479b09c93a0d33911e36a
TahaMirza50 <tahamirza50@gmail.com>
2024-03-16 22:09:48 +0500

    completed milestone and project apis

---
+++ src/app/api/student/project/[id]/[milestoneid]/route.js
@@ -0,0 +1,40 @@
+import { connectToDB } from "@/lib/utils";
+import Milestone from "@/models/Milestone";
+import Project from "@/models/Project";
+import { HttpStatusCode } from "axios";
+import { NextResponse } from "next/server";
+
+export async function POST(request) {
+    connectToDB();
+
+    try {
+        const projectID = request.params.id;
+        const milestoneID = request.params.milestoneid;
+        const body = await request.json();
+        const files = body.files;
+
+        const project = await Project.findById(projectID);
+
+        const milestoneToSubmit = Milestone.findById(milestoneID);
+
+        // if(milestoneToSubmit.deadline) check deadline here
+
+        const milestone = project.milestones.filter(milestone => milestone.ID === milestoneID);
+
+        if (milestone.completed)
+            return NextResponse.json({ message: 'Milestone already completed' }, { status: HttpStatusCode.OK });
+
+        const milestones = project.milestones.filter(milestone => milestone.ID !== milestoneID);
+        milestones.push(milestone);
+        project.milestones = milestones;
+
+        await project.save();
+
+        return NextResponse.json({ message: 'Milestone completed', project: project }, { status: HttpStatusCode.OK });
+
+
+    } catch (error) {
+        return NextResponse.json({ message: error.message }, { status: HttpStatusCode.INTERNAL_SERVER_ERROR });
+    }
+
+}
---
+++ src/app/api/student/project/get/route.js
@@ -0,0 +1,26 @@
+import { connectToDB } from "@/lib/utils";
+import Group from "@/models/Group";
+import Project from "@/models/Project";
+import Student from "@/models/Student";
+import { HttpStatusCode } from "axios";
+import { NextResponse } from "next/server";
+
+export async function GET(request){
+    connectToDB();
+
+    try {
+        const profileID = request.headers.get('profileID');
+
+        const student = await Student.findById(profileID);
+
+        if(student.group === null)
+            return NextResponse.json({ message: 'Student not in a group' }, { status: HttpStatusCode.OK });
+
+        const group = await Group.findById(student.group);
+        const project = await Project.findById(group.project);
+
+        return NextResponse.json(project, { status: HttpStatusCode.OK});
+    } catch (error) {
+        return NextResponse.json({ message: error.message }, { status: HttpStatusCode.INTERNAL_SERVER_ERROR });
+    }
+}
