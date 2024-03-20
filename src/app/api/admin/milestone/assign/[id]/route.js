3875b58c865f970d2da7417a0100148ce2512358
TahaMirza50 <tahamirza50@gmail.com>
2024-03-21 00:41:50 +0500

    remerge

--- src/app/api/admin/milestone/assign/[id]/route.js
+++ src/app/api/admin/milestone/assign/[id]/route.js
@@ -1,9 +1,7 @@
 import { connectToDB } from "@/lib/utils";
-import Milestone from "@/models/Milestone";
 import Project from "@/models/Project";
 import { HttpStatusCode } from "axios";
 import { NextResponse } from "next/server";
-import { finished } from "nodemailer/lib/xoauth2";
 
 export async function POST(request, { params }) {
     connectToDB();
