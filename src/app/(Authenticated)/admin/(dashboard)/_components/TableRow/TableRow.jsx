ff1ec2320ba78299450d3948934d482e34439046
Syed Hamza Akbar Ali <hamzakbarali@gmail.com>
2024-05-05 18:04:57 +0500

    Created Modal Component
    Created Modal Component

--- src/app/(Authenticated)/admin/(dashboard)/_components/TableRow/TableRow.jsx
+++ src/app/(Authenticated)/admin/(dashboard)/_components/TableRow/TableRow.jsx
@@ -4,8 +4,13 @@ import styles from "./TableRow.module.css";
 
 export default function TableRow({children}){
     return (
-        <tr className="border-b dark:border-gray-700 hover:bg-gray-100 cursor-pointer">
+        <tr 
+            className="border-b dark:border-gray-700 hover:bg-gray-100 cursor-pointer"  
+            onClick={() => console.log("Row clicked")}
+        >
+                     
             {children}
+            
         </tr>
     );
 }
