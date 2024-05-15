0804f629850cd89aa8b243f1f7f4f1601482af0a
Syed Hamza Akbar Ali <hamzakbarali@gmail.com>
2024-05-15 20:25:46 +0500

    fixed issues in backend_routes.js file
    fixed issues in backend_routes.js file

--- src/utils/routes/backend_routes.js
+++ src/utils/routes/backend_routes.js
@@ -1,37 +1,40 @@
 const baseURL = process.env.NEXT_PUBLIC_BACKEND_URL || "http://localhost:3000/api";
 
 export const BACKEND_ROUTES = {
-  login: ${baseURL}/auth/login,
-  logout: ${baseURL}/auth/logout,
-  refresh: ${baseURL}/auth/refresh,
+  login: `${baseURL}/auth/login`,
+  logout: `${baseURL}/auth/logout`,
+  refresh: `${baseURL}/auth/refresh`,
   sendRequest: 'http://localhost:3000/api/student/request/send',
-  getProfile: ${baseURL}/user/account/get,
-  getAllCompanies: ${baseURL}/user/company/get/all, // (user) companies
+  getProfile: `${baseURL}/user/account/get`,
+  getAllCompanies: `${baseURL}/user/company/get/all`, // (user) companies
 
-  updateProfileMentor: ${baseURL}/mentor/account/update,
-  updateProposalLinkMentor: ${baseURL}/mentor/proposal/updateLink,
-  deleteProposalMentor: ${baseURL}/mentor/proposal/delete,
-  getProposalsMentor: ${baseURL}/mentor/proposal/get,
-  createProposalMentor: ${baseURL}/mentor/proposal/create,
-  getMentorGroups: ${baseURL}/mentor/group/get/all,
-  getMentorGroupDetails: ${baseURL}/mentor/group/get/,
+  updateProfileMentor: `${baseURL}/mentor/account/update`,
+  updateProposalLinkMentor: `${baseURL}/mentor/proposal/updateLink`,
+  deleteProposalMentor: `${baseURL}/mentor/proposal/delete`,
+  getProposalsMentor: `${baseURL}/mentor/proposal/get`,
+  createProposalMentor: `${baseURL}/mentor/proposal/create`,
+  getMentorGroups: `${baseURL}/mentor/group/get/all`,
+  getMentorGroupDetails: `${baseURL}/mentor/group/get/`,
 
-  getReceivedRequest: ${baseURL}/user/request/get/received,
-  getGroupDetails: ${baseURL}/user/group/get,
-  deleteRequest: ${baseURL}/user/request/decline,
-  acceptRequest: ${baseURL}/user/request/accept,
+  getReceivedRequest: `${baseURL}/user/request/get/received`,
+  getGroupDetails: `${baseURL}/user/group/get`,
+  deleteRequest: `${baseURL}/user/request/decline`,
+  acceptRequest: `${baseURL}/user/request/accept`,
 
-  getAllMilestones: ${baseURL}/admin/milestone/get-all, // (admin) milestones
-  createMilestone: ${baseURL}/admin/milestone/create, // (admin) milestones
+  getAllMilestones: `${baseURL}/admin/milestone/get-all`, // (admin) milestones
+  createMilestone: `${baseURL}/admin/milestone/create`, // (admin) milestones
 
-  createUser: ${baseURL}/admin/account/create, // (admin) users
-  getUsers: ${baseURL}/admin/user/get/, // (admin) users
+  createUser: `${baseURL}/admin/account/create`, // (admin) users
+  getUsers: `${baseURL}/admin/user/get/`, // (admin) users
 
-  getAllProjects: ${baseURL}/admin/project/get-all, // (admin) projects
-  markProjectFinished: ${baseURL}/admin/project/mark-finished/, // (admin) projects
+  getAllProjects: `${baseURL}/admin/project/get-all`, // (admin) projects
+  markProjectFinished: `${baseURL}/admin/project/mark-finished/`, // (admin) projects
 
-  getFYPGroups: ${baseURL}/admin/group/get/all, // (admin) fyp groups
-  finalizeAllFYPGroups: ${baseURL}/admin/group/finalize/all, // (admin) fyp groups
+  getFYPGroups: `${baseURL}/admin/group/get/all`, // (admin) fyp groups
+  finalizeAllFYPGroups: `${baseURL}/admin/group/finalize/all`, // (admin) fyp groups
 
-  getAnnouncements: ${baseURL}/admin/notification/get/, // (admin) announcements
+  getAnnouncements: `${baseURL}/admin/notification/get/`, // (admin) announcements
+  createAnnouncement: `${baseURL}/admin/notification/create`, // (admin) announcements
+
+  createCompany: `${baseURL}/admin/company/create`, // (admin) companies
 };
