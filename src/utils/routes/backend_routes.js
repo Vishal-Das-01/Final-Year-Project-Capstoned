bf092177dcb724d38915c4ec96e23c35fc5c3f58
TahaMirza50 <tahamirza50@gmail.com>
2024-05-24 16:16:05 +0500

    completed proposals page for student

--- src/utils/routes/backend_routes.js
+++ src/utils/routes/backend_routes.js
@@ -48,6 +48,7 @@ export const BACKEND_ROUTES = {
 
   createCompany: `${baseURL}/admin/company/create`, // (admin) companies
 
+  studentRequestProposal: `${baseURL}/student/group/proposal/select-deselect`, // (student) proposals
   studentSubmitMilestone: `${baseURL}/student/project/`, // (student) milestones
   getStudentProjects: `${baseURL}/student/project/get`, // (student) projects
   studentGetAllProposals: `${baseURL}/student/proposal/get/all`, // (student) proposals
