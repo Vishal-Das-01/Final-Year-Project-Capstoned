86a52dae6bd92cb59f4af4e774b42ba5f1d74e63
Syed Hamza Akbar Ali <hamzakbarali@gmail.com>
2024-05-31 10:13:40 +0500

    Added capstoned logo to all dashboard pages
    Added capstoned logo to all dashboard pages

--- src/app/(Authenticated)/admin/(dashboard)/_components/Navbar/Navbar.jsx
+++ src/app/(Authenticated)/admin/(dashboard)/_components/Navbar/Navbar.jsx
@@ -3,6 +3,7 @@ import Brand from "@/components/Brand/Brand.jsx";
 import Avatar from "./_components/Avatar/Avatar.jsx";
 import NotificationIcon from "./_components/NotificationIcon/NotificationIcon.jsx";
 import {FRONTEND_ROUTES} from "@/utils/routes/frontend_routes";
+import CapstonedLogo from "@/components/CapstonedLogo/CapstonedLogo.jsx";
 
 export default function Navbar(props){
 	return (
@@ -12,6 +13,8 @@ export default function Navbar(props){
 
 				<div className={`${styles.leftContent} w-full h-full flex flex-row items-center justify-start `}>
 					
+					<CapstonedLogo />
+
 					<Brand href={FRONTEND_ROUTES.admin_dashboard_home_page}>
 						Capstoned
 					</Brand>
