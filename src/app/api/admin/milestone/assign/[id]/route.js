278428709c3e119cbbcc181c739935bccb41fba0
TahaMirza50 <tahamirza50@gmail.com>
2024-05-31 11:12:24 +0500

    fixed assign milestone pai

--- src/app/api/admin/milestone/assign/[id]/route.js
+++ src/app/api/admin/milestone/assign/[id]/route.js
@@ -15,34 +15,34 @@ export async function POST(request, { params }) {
 
         const milestoneToAssign = await Milestone.findById(id);
 
-        const allMilestones = await Milestone.find({year: milestoneToAssign.year, assigned: true});
+        const allMilestones = await Milestone.find({ year: milestoneToAssign.year, assigned: true });
 
         let percentage = 0;
-        for( const mile of allMilestones) {
+        for (const mile of allMilestones) {
             percentage = percentage + mile.percentage;
         }
 
-        if (percentage + milestone.percentage > 100) {
-            return NextResponse.json({ message: 'The total weightage of milestones cannot exceed 100%' }, { status: HttpStatusCode.BadRequest });
+        if (percentage + milestoneToAssign.percentage > 100) {
+            return NextResponse.json({ message: `The total weightage of ${percentage + milestoneToAssign.percentage} milestones cannot exceed 100%` }, { status: HttpStatusCode.BadRequest });
         }
 
-        const projects = await Project.find({finished: false})
-                    .populate('milestones', 'milestoneID')
-                    .populate("group","lead members")
+        const projects = await Project.find({ finished: false, year: milestoneToAssign.year })
+            .populate('milestones', 'milestoneID')
+            .populate("group", "lead members")
 
-        for(const project of projects){
+        for (const project of projects) {
 
-            if(project.milestones.find(milestone => milestone.milestoneID == id)){
+            if (project.milestones.find(milestone => milestone.milestoneID == id)) {
                 continue;
             }
             const marks = [];
-            marks.push({member: project.group.lead, marks: 0});   
+            marks.push({ member: project.group.lead, marks: 0 });
 
-            for(const member of project.group.members){
-                marks.push({member: member, marks: 0});
+            for (const member of project.group.members) {
+                marks.push({ member: member, marks: 0 });
             }
 
-            const assignedMilestone =  new AssignedMilestone({
+            const assignedMilestone = new AssignedMilestone({
                 projectID: project._id,
                 milestoneID: id,
                 marks: marks
@@ -52,10 +52,10 @@ export async function POST(request, { params }) {
             await project.save();
         }
 
-        const milestone = await Milestone.findByIdAndUpdate(id, {assigned: true});
-        
+        const milestone = await Milestone.findByIdAndUpdate(id, { assigned: true });
+
         return NextResponse.json({ message: 'Milestones assigned successfully' }, { status: HttpStatusCode.Ok });
-        
+
     } catch (error) {
         console.log(error);
         return NextResponse.json({ message: 'Error assigning milestones' }, { status: HttpStatusCode.InternalServerError });
